bf5e4497f96cb04310536c9a4dd2f3628405aef5
Teron Sampson <32195526+sam3106@users.noreply.github.com>
2018-11-20 19:33:57 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var map;
 var infoWindow;
 var request;
 var center = new google.maps.LatLng(43.655089, -70.264527);
-var markerArray = [];
+var markerArray=[];
 
 function initialize() {
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -39,6 +39,7 @@ function initialize() {
     /* This function creates my google map. Also the variable center is used to create the starting point for the google maps using latitud and longitude.  */
   });
 
+  
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
@@ -67,16 +68,14 @@ function initialize() {
   var service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, callback);
   function callback(results, status) {
-    markerArray = [];
+    markerArray=[];
     for (var i = 0; i < results.length; i++) {
       createMarker(results[i]);
     }
-
+  
     // Add a marker clusterer to manage the markers. Cluster api reference is added in index.html
-    var markerCluster = new MarkerClusterer(map, markerArray, {
-      imagePath:
-        'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-    });
+    var markerCluster = new MarkerClusterer(map, markerArray, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
   }
   function createMarker(place) {
     //alert(JOSN.stringify(place));
@@ -97,7 +96,7 @@ function initialize() {
         fontSize: '12px',
         color: 'black',
         // greater the value more bold it will be
-        fontWeight: '600',
+        fontWeight: "600",
         text: place.name
       },
 
@@ -106,43 +105,102 @@ function initialize() {
 
     // add each marker to array so that we can pass this array for clustering
     markerArray.push(marker);
-
+    
     // marker click event with dynamic content
     google.maps.event.addListener(marker, 'click', function() {
-      var geocoder = new google.maps.Geocoder();
-      var address = '';
+      var geocoder = new google.maps.Geocoder;
+      var address="";
 
       // geocode the place id to  get the complete address of location
-      geocoder.geocode({ placeId: place.place_id }, function(results, status) {
-        // current location to marker location direction url
-        var dirURL =
-          'https://www.google.com/maps/dir/?api=1&destination=' +
-          results[0].geometry.location.lat().toString() +
-          ',' +
-          results[0].geometry.location.lng().toString();
+      geocoder.geocode({'placeId': place.place_id},function(results, status){
 
+        // current location to marker location direction url
+        var dirURL="https://www.google.com/maps/dir/?api=1&destination="+results[0].geometry.location.lat().toString()+","+results[0].geometry.location.lng().toString();
+        
         if (status === 'OK') {
           if (results[0]) {
-            address =
-              '<strong>' +
-              results[0].formatted_address +
-              "</strong><br><a  target='_blank' href='" +
-              dirURL +
-              "'> Get Directions </a>";
-          } else {
-            address = 'Address not available.';
+          address="<strong>"+results[0].formatted_address+"</strong><br><a  target='_blank' href='"+dirURL+"'> Get Directions </a>";
           }
-        } else {
-          address = 'Geocoder failed due to: ' + status;
+          else{
+            address="Address not available.";
+          }
         }
-
+        else{
+          address='Geocoder failed due to: ' + status
+        }
+        
         infoWindow.setContent(address);
       });
       infoWindow.open(map, this);
     });
+
+    
   }
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
+
+  searchBox.addListener('places_changed', function() {
+    
+    var places = searchBox.getPlaces();
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log('Returned place contains no geometry');
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+
+      };
+         var marker = new google.maps.Marker({
+      map: map,
+      icon: icon,
+      zoom: 19,
+      label: {
+        // you can change text size here
+        fontSize: '12px',
+        color: 'black',
+        // greater the value more bold it will be
+        fontWeight: "600",
+        text: place.name
+      },
+
+      position: place.geometry.location
+    });
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    
+    map.fitBounds(bounds);
+  });
+  
 }
 
 google.maps.event.addDomListener(
